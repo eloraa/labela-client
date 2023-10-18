589b61a830e5127b14cd80972560e0ac61bf4e10
elora <108570316+eloraistired@users.noreply.github.com>
2023-10-18 14:33:10 +0100

    Added user authintication

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,17 +1,25 @@
 import { Link, NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
+import { AuthContext } from '../providers/AuthProviders';
+import { toast } from 'react-toastify';
 
 export const Header = ({ className }) => {
-  const [navOpen, setNavOpen] = useState(false)
+  const [navOpen, setNavOpen] = useState(false);
+  const { user, signOutUser } = useContext(AuthContext);
 
+  const handleSignOut = () => {
+    signOutUser()
+      .then(() => toast('Signed out successfully.'))
+      .catch(() => toast('An error occurred. Please try again later'));
+  };
 
   return (
     <header className={`py-6 md:px-10 px-5 flex items-center text-sm justify-between sticky top-0 z-[999] ${className}`}>
       <Link to="/">
         <h1 className="font-bold uppercase text-base flex items-center gap-2 relative z-[9999]">
-          <div className='w-8 h-8'>
-            <svg viewBox="0 0 31 31" className='w-full h-full'>
+          <div className="w-8 h-8">
+            <svg viewBox="0 0 31 31" className="w-full h-full">
               <path
                 fillRule="evenodd"
                 clipRule="evenodd"
@@ -23,22 +31,54 @@ export const Header = ({ className }) => {
           Labela
         </h1>
       </Link>
-      <div className='absolute inset-0 backdrop-blur bg-white/80 md:-z-20'></div>
-      <button onClick={() => setNavOpen(!navOpen)} className='md:hidden border-b border-black active:scale-[.98] transition-transform relative z-[9999]'>{navOpen ? 'Close' : 'Menu'}</button>
-      <div className={`flex gap-10 flex-col -z-10 fixed inset-0 bg-white/80 bottom-0 top-20 px-5 backdrop-blur h-[calc(100vh-5rem)] pt-12 md:flex-row md:static md:h-auto md:p-0 transition-transform duration-500 ${navOpen ? 'translate-y-0' : 'max-md:-translate-y-[calc(100%+5rem)]'}`}>
-        <ul className="flex gap-6 max-md:w-full justify-between max-md:flex-col">
-          <li className='max-md:border-b max-md:py-4'>
-            <NavLink to="/add" className={({ isActive }) => (isActive ? 'border-b border-black' : '')}>
-              Add Product
-            </NavLink>
-          </li>
-          <li className='max-md:border-b max-md:py-4'>
-            <NavLink to="/cart" className={({ isActive }) => (isActive ? 'border-b border-black' : '')}>
-              My Cart
-            </NavLink>
-          </li>
-        </ul>
-        <Link className='max-md:w-full max-md:bg-black max-md:py-2 rounded max-md:font-bold max-md:text-white text-center' to="/signin">Sign In</Link>
+      <div className="absolute inset-0 backdrop-blur bg-white/80 md:-z-20"></div>
+      <button onClick={() => setNavOpen(!navOpen)} className="md:hidden border-b border-black active:scale-[.98] transition-transform relative z-[9999]">
+        {navOpen ? 'Close' : 'Menu'}
+      </button>
+      <div
+        className={`flex gap-10 flex-col -z-10 fixed inset-0 bg-white/80 bottom-0 top-20 px-5 backdrop-blur h-[calc(100vh-5rem)] pt-12 md:flex-row md:static md:h-auto md:p-0 transition-transform duration-500 ${
+          navOpen ? 'translate-y-0' : 'max-md:-translate-y-[calc(100%+5rem)]'
+        }`}
+      >
+        {((location.pathname === '/signin' && location.pathname === '/signup') || user) && (
+          <ul className="flex gap-6 max-md:w-full justify-between max-md:flex-co items-center">
+            <li className="max-md:border-b max-md:py-4">
+              <NavLink to="/add" className={({ isActive }) => (isActive ? 'border-b border-black' : '')}>
+                Add Product
+              </NavLink>
+            </li>
+            <li className="max-md:border-b max-md:py-4">
+              <NavLink to="/cart" className={({ isActive }) => (isActive ? 'border-b border-black' : '')}>
+                My Cart
+              </NavLink>
+            </li>
+          </ul>
+        )}
+
+        {user ? (
+          <div className="flex-1 justify-end items-center flex gap-4">
+            <div className="flex gap-2 items-center">
+              <Link to="/profile" className="w-8 h-8 overflow-hidden rounded-full bg-[#ddd]">
+                <img className="w-full h-full object-cover" src={user.photoURL ? user.photoURL : '/pfp-placeholder.png'} alt="" />
+              </Link>
+              <h2 className="text-ellipsis whitespace-nowrap leading-3">{user?.displayName ? user.displayName : user.email}</h2>
+            </div>
+            <div className="w-3 border-b border-black rounded"></div>
+            <button onClick={handleSignOut}>Sign Out</button>
+          </div>
+        ) : (
+          <div>
+            {location.pathname === '/signin' ? (
+              <Link state={location?.state} to="/signup">
+                Sign Up
+              </Link>
+            ) : (
+              <Link state={location?.state} to="/signin">
+                Sign In
+              </Link>
+            )}
+          </div>
+        )}
       </div>
     </header>
   );
