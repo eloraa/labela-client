9faac6f67140095c334fde71c26907706b9c1160
elora <108570316+eloraistired@users.noreply.github.com>
2023-10-19 09:08:01 +0100

    Added the adevertisement slider

--- src/components/pages/Brand.jsx
+++ src/components/pages/Brand.jsx
@@ -18,8 +18,8 @@ export const Brand = () => {
           <SwiperSlide key={i}>
             <div className="w-full h-full bg-black bg-cover bg-center md:px-10 px-5 flex items-end pb-10" style={{ backgroundImage: `url(/${params.brand.toLowerCase()}${scheme.image})` }}>
               <div className="text-2xl md:text-4xl leading-[1] font-black uppercase heading flex flex-col items-start">
-                <h1 className={`bg-[${scheme.theme}] inline-block`}>{scheme.heading[0]}</h1>
-                <h1 className={`bg-[${scheme.theme}] inline-block`}>{scheme.heading[1]}</h1>
+                <h1 className='inline-block' style={{ backgroundColor: scheme.theme }}>{scheme.heading[0]}</h1>
+                <h1 className='inline-block' style={{ backgroundColor: scheme.theme }}>{scheme.heading[1]}</h1>
               </div>
             </div>
           </SwiperSlide>
