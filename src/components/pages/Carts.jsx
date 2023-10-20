98aa8ed7c262314e8a15adce86da9240deeeb48c
elora <108570316+eloraistired@users.noreply.github.com>
2023-10-20 13:28:30 +0100

    added a checkout button

--- src/components/pages/Carts.jsx
+++ src/components/pages/Carts.jsx
@@ -10,12 +10,13 @@ export const Carts = () => {
       <h1 className="text-2xl font-black uppercase mb-12">My Cart</h1>
 
       <div className="grid md:grid-cols-2 2xl:grid-cols-3 gap-10">
-        {carts.length ? (
-          carts.map((product, index) => <CartProduct key={index} product={product}></CartProduct>)
-        ) : (
-          <div>You have no products in your Cart.</div>
-        )}
+        {carts.length ? carts.map((product, index) => <CartProduct key={index} product={product}></CartProduct>) : <div>You have no products in your Cart.</div>}
       </div>
+      {carts.length ? (
+        <button className="bg-black dark:bg-dark dark:text-black mt-16 py-2.5 md:px-24 px-0 text-white font-bold rounded active:scale-[.99] transition-transform text-sm">Checkout</button>
+      ) : (
+        ''
+      )}
     </main>
   );
 };
