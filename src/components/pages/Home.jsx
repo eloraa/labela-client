16b7030e6d45fc81745eddd6b0e8f1b77db9961a
elora <108570316+eloraistired@users.noreply.github.com>
2023-10-21 19:37:18 +0100

    removed unnecessary margin

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -40,7 +40,7 @@ export const Home = () => {
         </SwiperSlide>
       </Banner>
 
-      <main className="md:px-10 px-5 py-10 mt-40 dark:text-white">
+      <main className="md:px-10 px-5 py-10 mt-10 dark:text-white">
         <div className="mb-20">
           <h1 className="text-2xl font-black uppercase mb-12">Latest Collection</h1>
 
